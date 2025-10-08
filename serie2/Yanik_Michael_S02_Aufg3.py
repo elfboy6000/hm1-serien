7aea4229b84265dd33acd3eff71f186ce2b3faf7
Dobeson Michael (dobesmic) <dobesmic@students.zhaw.ch>
2025-10-08 11:29:35 +0200

    Serie 2 Aufg 3 fixed

--- serie2/Yanik_Michael_S02_Aufg3.py
+++ serie2/Yanik_Michael_S02_Aufg3.py
@@ -1,53 +1,43 @@
 import math
 import matplotlib.pyplot as plt
 
-# a)
 def side_calculator(prev_side_length):
     return math.sqrt(2 - 2 * math.sqrt(1 - (prev_side_length / 2) ** 2))
 
-sides = 6;
-prev_side_length = 1
-
-plt.figure(figsize=(10, 10))
-plt.title("Side length of inscribed polygon vs number of sides")
-plt.xlabel("Number of sides")
-plt.ylabel("Side length")
-x = [sides * 2**i for i in range(0, 20)]
-y = []
-current = prev_side_length
-for i in range(0, 20):
-    y.append(current)
-    current = side_calculator(current)
-plt.plot(x, y, marker='o')
-plt.xscale('log', base=2)
-plt.yscale('linear')
-plt.grid(True, which="both", ls="--")
-plt.tight_layout()
-plt.show()
-
-# Was passiert für grosse n und weshalb?
-# The side length approaches 0 as the number of sides increases, because the inscribed polygon approaches the shape of the circle.
-
-# b)
 def new_side_calculator(prev_side_length):
-    return math.sqrt(prev_side_length ** 2 / 2 * (1 + math.sqrt(1 - (prev_side_length / 2) ** 2)))
+    return math.sqrt(prev_side_length ** 2 / (2 * (1 + math.sqrt(1 - (prev_side_length / 2) ** 2))))
 
-plt.figure(figsize=(10, 10))
-plt.title("New Side length of inscribed polygon vs number of sides")
+sides = 6;
+prev_side_length = 1.0
+
+plt.figure()
+plt.title("Perimeter of inscribed polygon vs number of sides")
 plt.xlabel("Number of sides")
-plt.ylabel("Side length")
-x = [sides * 2**i for i in range(0, 20)]
+plt.ylabel("Perimeter")
+x = []
 y = []
+x2 = []
+y2 = []
 current = prev_side_length
-for i in range(0, 20):
-    y.append(current)
+for i in range(0, 30):
+    x.append(sides)
+    x2.append(sides)
+    y.append(prev_side_length * sides)
+    y2.append(current * sides)
+    prev_side_length = side_calculator(prev_side_length)
     current = new_side_calculator(current)
-plt.plot(x, y, marker='o')
+    sides *= 2
+plt.plot(x, y, marker='o', label="f(x)")
+plt.plot(x2, y2, marker='o', label="f(y)")
+plt.axhline(2 * math.pi, linestyle="--", label="2π")
 plt.xscale('log', base=2)
-plt.yscale('linear')
 plt.grid(True, which="both", ls="--")
+plt.legend()
 plt.tight_layout()
 plt.show()
 
-# Was beobachten Sie?
-# The side length approaches 0 much slower and more linearly
+# a) Was passiert für grosse n und weshalb?
+# The perimeter starts getting bigger 2π, reaching around 12, before dropping all the way to 0.
+
+# b) Was beobachten Sie?
+# The perimeter reaches 2π, but stays at that value, unlike the first method.
